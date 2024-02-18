cc5019285856a03bd507f75d1766677b29e97e6c
Parkash Arjan <Arjan.Parkash.Git@gmail.com>
2024-02-17 20:49:09 -0500

    spring rest repo

--- src/main/java/com/spring/rest/repo/SpringRestRepositoryApplication.java
+++ src/main/java/com/spring/rest/repo/SpringRestRepositoryApplication.java
@@ -3,8 +3,6 @@ package com.spring.rest.repo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
--- src/main/java/com/spring/rest/repo/StudentRepository.java
+++ src/main/java/com/spring/rest/repo/StudentRepository.java
@@ -4,6 +4,6 @@ import com.spring.rest.model.Student;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "students" , path = "students")
-public interface StudentRepository extends PagingAndSortingRepository<Student,Integer> {
+@RepositoryRestResource(collectionResourceRel = "students", path = "students")
+public interface StudentRepository extends PagingAndSortingRepository<Student, Integer> {
 }
